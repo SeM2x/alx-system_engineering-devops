06cbec05485866355686bf57e2ad6ae71c9c1e5b
SeM2x <houssem135.302@gmail.com>
2024-03-28 00:20:13 +0100

    7-puppet_install_nginx_web_server.pp

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 # script that configures a new Ubuntu machine to respect above requirements (this script will be run on the server itself
 package { 'nginx':
-	ensure => installed,
+  ensure => installed,
 }
 
-file { '/var/www/html/index.html': 
-	ensure  => present,
-	content => "Hello World!\n"
+file { '/var/www/html/index.html':
+  ensure  => present,
+  content => "Hello World!\n"
 }
 
 exec {'sed':
-	command => "/usr/bin/sed -i '24i\\	rewrite ^/redirect_me localhost permanent;' /etc/nginx/sites-available/default"
+  command => "/usr/bin/sed -i '24i\\	rewrite ^/redirect_me localhost permanent;' /etc/nginx/sites-available/default"
 }
 
 exec {'service':
-	command => '/usr/sbin/service nginx start'
+  command => '/usr/sbin/service nginx start'
 }
