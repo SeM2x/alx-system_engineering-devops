21897f86bc70d606782be4a85a86f65065cd30bb
SeM2x <houssem135.302@gmail.com>
2024-04-01 18:41:32 +0100

    1-install_load_balancer

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,22 +1,24 @@
 # Configures a brand new Ubuntu machine
-package { 'nginx':
-  ensure => installed,
+exec {'update':
+  provider => shell,
+  command  => 'sudo apt-get -y update',
+  before   => Exec['install Nginx'],
 }
 
-file { '/var/www/html/index.html':
-  ensure  => present,
-  content => "Hello World!\n"
+exec {'install Nginx':
+  provider => shell,
+  command  => 'sudo apt-get -y install nginx',
+  before   => Exec['add_header'],
 }
 
-exec {'redirect':
-  command => "/usr/bin/sed -i '24i\\	rewrite ^/redirect_me localhost permanent;' /etc/nginx/sites-available/default"
-}
-
-exec {'add_header':
+exec { 'add_header':
+  provider    => shell,
   environment => ["HOST=${hostname}"],
-  command => '/usr/bin/sed -i "25i\\        add_header X-Served-By \"$HOST\";" /etc/nginx/sites-available/default'
+  command     => 'sudo sed -i "s/include \/etc\/nginx\/sites-enabled\/\*;/include \/etc\/nginx\/sites-enabled\/\*;\n\tadd_header X-Served-By \"$HOST\";/" /etc/nginx/nginx.conf',
+  before      => Exec['restart Nginx'],
 }
 
-exec {'service':
-  command => '/usr/sbin/service nginx start'
+exec { 'restart Nginx':
+  provider => shell,
+  command  => 'sudo service nginx restart',
 }
