2a7bfb3f6a840f2ab6e46364b47abfc2192835d7
SeM2x <houssm135.302@gmail.com>
2024-07-29 22:04:30 +0100

    0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -5,7 +5,7 @@ import sys
 
 if __name__ == "__main__":
     user = requests.get(f"https://jsonplaceholder.typicode.com/users/{sys.argv[1]}").json()
-    todos = requests.get(f"https://jsonplaceholder.typicode.com/todos?userId={sys.argv[1]}").json()
+    todos = requests.get("https://jsonplaceholder.typicode.com/todos", params={"userId": sys.argv[1]}).json()
     completed = [todo.get("title")for todo in todos if bool(todo.get("completed"))]
     print(f"Employee {user.get("name")} is done with tasks({
         len(completed)}/{len(todos)}):")
