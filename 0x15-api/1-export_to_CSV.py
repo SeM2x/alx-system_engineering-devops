854de758cb79d0254c47b38a8da5d1f24b601844
SeM2x <houssm135.302@gmail.com>
2024-07-29 23:05:22 +0100

    1-export_to_CSV.py

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""for a given employee ID, returns information
+about his/her TODO list progress."""
+import csv
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
+    todos = requests.get(url + "/todos", params={"userId": sys.argv[1]}).json()
+    data = [[sys.argv[1], user.get('username'), todo.get(
+        'completed'), todo.get('title')] for todo in todos]
+    with open('data.csv', mode='w', newline='') as file:
+        csv_writer = csv.writer(file)
+        csv_writer.writerows(data)
