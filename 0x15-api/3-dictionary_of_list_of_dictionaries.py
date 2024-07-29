f84fc0537826b47589a7bf7cc9bb2343661ea25c
SeM2x <houssm135.302@gmail.com>
2024-07-29 23:43:57 +0100

    3-dictionary_of_list_of_dictionaries.py

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -9,7 +9,11 @@ if __name__ == "__main__":
     users = requests.get(url + "users").json()
     data = {}
     for user in users:
-        todos = requests.get(url + "/todos", params={"userId": user.get('id')}).json()
-        data[user.get('id')] = [{"username": user.get('username'), "task": todo.get('title'), "completed": todo.get('completed')} for todo in todos]
+        todos = requests.get(
+            url + "/todos", params={"userId": user.get('id')}).json()
+        data[user.get('id')] = [{"username": user.get('username'),
+                                 "task": todo.get('title'),
+                                 "completed": todo.get('completed')}
+                                for todo in todos]
     with open('todo_all_employees.json', 'w') as file:
         json.dump(data, file)
