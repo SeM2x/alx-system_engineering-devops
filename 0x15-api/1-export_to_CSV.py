108510672ca868574f16c8d14b7c907362615704
SeM2x <houssm135.302@gmail.com>
2024-07-29 23:08:58 +0100

    1-export_to_CSV.py

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -12,5 +12,5 @@ if __name__ == "__main__":
     data = [[sys.argv[1], user.get('username'), todo.get(
         'completed'), todo.get('title')] for todo in todos]
     with open(sys.argv[1] + '.csv', mode='w', newline='') as file:
-        csv_writer = csv.writer(file)
+        csv_writer = csv.writer(file, quoting=csv.QUOTE_ALL)
         csv_writer.writerows(data)
