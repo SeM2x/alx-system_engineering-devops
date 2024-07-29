87cd0518c5246b8c3d421e67e981c2836e5f5a75
SeM2x <houssm135.302@gmail.com>
2024-07-29 23:41:45 +0100

    3-dictionary_of_list_of_dictionaries.py

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""for a given employee ID, returns information
+about his/her TODO list progress."""
+import json
+import requests
+
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/"
+    users = requests.get(url + "users").json()
+    data = {}
+    for user in users:
+        todos = requests.get(url + "/todos", params={"userId": user.get('id')}).json()
+        data[user.get('id')] = [{"username": user.get('username'), "task": todo.get('title'), "completed": todo.get('completed')} for todo in todos]
+    with open('todo_all_employees.json', 'w') as file:
+        json.dump(data, file)
