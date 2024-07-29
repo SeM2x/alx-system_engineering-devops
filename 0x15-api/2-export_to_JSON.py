ec2b57410a20cdabeaaca200df2b5838d2685a02
SeM2x <houssm135.302@gmail.com>
2024-07-29 23:33:05 +0100

    2-export_to_JSON.py

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""for a given employee ID, returns information
+about his/her TODO list progress."""
+import json
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
+    todos = requests.get(url + "/todos", params={"userId": sys.argv[1]}).json()
+    data = {sys.argv[1]: [{"task": todo.get('title'), "completed": todo.get(
+        'completed'), "username": user.get('username'), } for todo in todos]}
+    with open('file.json', 'w') as file:
+        json.dump(data, file)
