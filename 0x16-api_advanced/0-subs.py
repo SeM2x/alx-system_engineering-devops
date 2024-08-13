a94524535c04165972a4323dc6845c8b38bc4f43
SeM2x <houssem135.302@gmail.com>
2024-08-13 22:14:46 +0100

    0-subs.py

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""module that define number_of_subscribers(subreddit) function"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """Function that queries the Reddit API
+    and returns the number of subscribers"""
+    request = requests.get(
+        'https://www.reddit.com/r/{}/about.json'.format(subreddit))
+    count = request.json().get('data').get('subscribers')
+    if count is None:
+        return 0
+    return count
