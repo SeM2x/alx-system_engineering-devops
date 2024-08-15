8c650bcf777653d8ca3d7055e9194dab51c04c29
SeM2x <houssem135.302@gmail.com>
2024-08-15 11:55:32 +0100

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -11,11 +11,14 @@ def number_of_subscribers(subreddit):
     Function that queries the Reddit API and returns
     the number of subscribers for a given subreddit.
     """
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    res = requests.get(url, allow_redirects=False,
-                       headers={"User-Agent": "Custom"},)
+
+    res = requests.get(
+        "https://www.reddit.com/r/{}/about.json".format(subreddit),
+        headers={"User-Agent": "Custom"},
+        allow_redirects=False
+    )
     if res.status_code == 200:
         data = res.json()
-        return data.get('data').get('subscribers')
+        return int(data.get('data').get('subscribers'))
     else:
         return 0
