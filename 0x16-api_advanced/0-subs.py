99a2c4f18851ffc27c71ef59fddf1335b96dd388
SeM2x <houssem135.302@gmail.com>
2024-08-15 12:01:01 +0100

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -14,7 +14,7 @@ def number_of_subscribers(subreddit):
 
     res = requests.get(
         "https://www.reddit.com/r/{}/about.json".format(subreddit),
-        headers={"User-Agent": "Custom"},
+        headers={"User-Agent": "Mozilla/5.0"},
         allow_redirects=False
     )
     if res.status_code == 200:
